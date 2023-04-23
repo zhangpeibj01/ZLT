22fc12504fd3a5ac41c8897183c73b278fde6ddb
zhangpeibj01 <zhangpeibj01@yuanfudao.com>
2023-04-23 16:04:10 +0800

    fix

--- Sources/App/OpenAIClient.swift
+++ Sources/App/OpenAIClient.swift
@@ -10,7 +10,7 @@ import OpenAIKit
 import Vapor
 
 public class OpenAIClient {
-    public static func requestForImage(prompt: String, count: Int, height: Int, width: Int) async throws -> AIImage {
+    public static func requestForImage(prompt: String, count: Int, size: String) async throws -> AIImage {
         let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
 
         let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup))
@@ -19,10 +19,27 @@ public class OpenAIClient {
             try? httpClient.syncShutdown()
         }
 
-        let configuration = Configuration(apiKey: "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")
+        let configuration = Configuration(apiKey: "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")
         let openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
-        let image = try await openAIClient.images.create(prompt: "Tiger Woods eating soup")
+        let image = try await openAIClient.images.create(prompt: prompt, n: count, size: AIImageSize(rawValue: size)?.size ?? .tenTwentyFour)
         return .init(data: image.data.map { .init(url: $0.url) })
+    }
+}
+
+enum AIImageSize: String {
+    case small
+    case medium
+    case large
+
+    var size: Image.Size {
+        switch self {
+        case .small:
+            return .twoFiftySix
+        case .medium:
+            return .fiveTwelve
+        case .large:
+            return .tenTwentyFour
+        }
     }
 }
 
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,13 +13,11 @@ func routes(_ app: Application) throws {
             throw Abort(.badRequest)
         }
         let count: Int = req.query["count"] ?? 1
-        let height: Int = req.query["height"] ?? 1024
-        let width: Int = req.query["width"] ?? 1024
+        let size: String = req.query["size"] ?? "1024x1024"
         return try await OpenAIClient.requestForImage(
             prompt: prompt,
             count: count,
-            height: height,
-            width: width
+            size: size
         )
     }
 }
